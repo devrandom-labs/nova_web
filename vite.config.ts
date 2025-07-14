abb773e542e2fa71240ec14086a18d0859dc14b3
Joel DSouza <joeldsouzax@gmail.com>
2025-07-14 16:17:16 +0200

    chore: update PWA configuration and project name in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,34 +19,12 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
-        name: 'Anker',
-        short_name: 'Anker',
+        name: 'Nova - devrandom.co',
+        short_name: 'Nova',
         theme_color: '#FBCF60',
         icons: [
-          {
-            src: 'pwa-64x64.png',
-            sizes: '64x64',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any',
-          },
-          {
-            src: 'maskable-icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
+          
         ],
       },
     }),
@@ -57,10 +35,6 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: (id: string) => {
-          if (id.includes('toastify')) {
-            return '@vendor-react-toastify';
-          }
-          
           if (id.includes('react-router') || id.includes('react-router')) {
             return '@vendor-react-router';
           }
